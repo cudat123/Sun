c2dce3155d9a214877be403685b20efb94b09a45
cudat123 <btdatmodlo@gmail.com>
2026-02-21 14:56:04 +0700

    Change endpoint from '/ttsun' to '/api/sun'

--- apisun.js
+++ apisun.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const app = express();
 const PORT = 3000;
 
-app.get("/ttsun", async (req, res) => {
+app.get("/api/sun", async (req, res) => {
     try {
         const response = await axios.get("https://sunlaymaynkx.hacksieucap.pro/ttsunver2");
         const data = response.data;
